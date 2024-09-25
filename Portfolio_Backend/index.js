acc296c1d256cc4d366c1d3fd7c407402ccf1d67
ShafiqueBughio <153521292+ShafiqueBughio@users.noreply.github.com>
2024-09-25 20:45:48 +0500

    Update index.js

--- Portfolio_Backend/index.js
+++ Portfolio_Backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors(
   {
-    origin:["https://portfoliofrontend-azure.vercel.app/"],
+    origin:["https://portfoliofrontend-azure.vercel.app"],
     methods:["POST","GET"],
     credentials:true,
   }
