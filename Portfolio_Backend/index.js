c4a80da88e865dc8929be476e11b72759171291e
Shafique Bughio <shafiquebughio153@gmail.com>
2024-09-26 21:25:19 +0500

    add new changes

--- Portfolio_Backend/index.js
+++ Portfolio_Backend/index.js
@@ -4,26 +4,38 @@ const port = process.env.PORT||4000;
 const {Connect_MongoDB} = require("./Conection")
 const router = require("./Routes/routes")
 const cors = require("cors");
-const bodyParser = require('body-parser');
+
 
 
 const app = express();
 
-app.use(cors(
-  {
-    origin:["https://portfoliofrontend-azure.vercel.app",
+// app.use(cors(
+//   {
+//     origin:["https://portfoliofrontend-azure.vercel.app",
    
-    ],
+//     ],
+//     methods: ["POST", "GET", "OPTIONS"],
+//     credentials:true,
+//   }
+// ));
+
+
+app.use(cors({
+    origin: ['https://portfoliofrontend-azure.vercel.app'], // Local and production origins
     methods: ["POST", "GET", "OPTIONS"],
-    credentials:true,
-  }
-));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+    credentials: true // Enable credentials (cookies, authorization headers, etc.)
+  }));
+
+// app.use(cors());
+
+app.use(express.json())
+
+app.use(express.static('public'));
+
+const dbConnectionString = process.env.MONGODB_URI || "mongodb+srv://shafique63005:pass%40portfolio@cluster1.ez7pc.mongodb.net/";
+Connect_MongoDB(dbConnectionString);
 
 
-
-Connect_MongoDB("mongodb+srv://shafique63005:pass%40portfolio@cluster1.ez7pc.mongodb.net/");
 
 app.use(router);
 
