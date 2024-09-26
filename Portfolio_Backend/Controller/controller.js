0ec1693256fdc318072d9dfbae06d1051956082b
Shafique Bughio <shafiquebughio153@gmail.com>
2024-09-26 21:44:50 +0500

    remove gmail

--- Portfolio_Backend/Controller/controller.js
+++ Portfolio_Backend/Controller/controller.js
@@ -1,5 +1,5 @@
 const { Portfolio_Model } = require("../Model/Hiring_Db");
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 // Handler
@@ -16,38 +16,12 @@ async function Handle_Form_Submission(req, res) {
             message,
         });
 
-        // Create a transporter object using SMTP transport
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user:  process.env.EMAIL,
-                pass: process.env.EMAIL_PASSWORD, // Use app-specific password
-            },
-        });
-
-        // Set up email data
-        const mailOptions = {
-            from: email, // Sender address (user's email)
-            to: 'shafiquebughio153@gmail.com', // Receiver address (your email)
-            subject: `New Contact Request from ${name}`,
-            text: message,
-            html: `<p><strong>Name:</strong> ${name}</p>
-                   <p><strong>Email:</strong> ${email}</p>
-                   <p><strong>Phone:</strong> ${phone}</p>
-                   <p><strong>Message:</strong></p>
-                   <p>${message}</p>`,
-        };
-
-        // Send email
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.error('Error sending email:', error.message);
-                return res.status(500).send('An error occurred while sending email.');
-            }
-
-            console.log('Email sent:', info.response);
-            return res.status(200).send('Form submitted successfully.');
-        });
+       if(user){
+       res.status(200).json("user created");
+       }
+       else{
+        res.status(500).json("user not created");
+       }
 
     } catch (error) {
         console.error('An error occurred:', error.message);
